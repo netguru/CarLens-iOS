ba8fbe8ef14238380f457ffacce8f7bf875226e0
Michał Kwiecień <michal.kwiecien@netguru.pl>
2018-05-29 12:56:25 +0200

    Initial Commit

---
+++ CarRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarRecognition
+//
+//  Created by Michał Kwiecień on 29/05/2018.
+//  Copyright © 2018 Netguru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
