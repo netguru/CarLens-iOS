88c690ea82baadd6ab21d8d65d082dafb57e13be
Michał Kwiecień <michal.kwiecien@netguru.pl>
2018-06-08 14:12:32 +0200

    Removed not longer needed responses

--- CarRecognition/Source Files/Modules/Live video/LiveVideoView.swift
+++ CarRecognition/Source Files/Modules/Live video/LiveVideoView.swift
@@ -5,8 +5,6 @@
 
 
 import UIKit
-import ARKit
-import SpriteKit
 
 internal final class LiveVideoView: View, ViewSetupable {
 
@@ -46,6 +44,7 @@ internal final class LiveVideoView: View, ViewSetupable {
     /// Label with time interval of last analyze
     lazy var analyzeTimeLabel: UILabel = {
         let view = UILabel()
+        view.bounds.size = CGSize(width: 10, height: 10)
         view.font = .systemFont(ofSize: 14)
         view.numberOfLines = 0
         view.textAlignment = .center
