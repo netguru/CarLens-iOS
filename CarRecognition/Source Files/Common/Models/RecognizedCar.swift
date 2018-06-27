7a53319599fa2a6e6391ddf6785924c72917ae36
Michał Kwiecień <michal.kwiecien@netguru.pl>
2018-06-27 16:29:32 +0200

    Pinning AR label to the detected cars

--- CarRecognition/Source Files/Common/Models/RecognizedCar.swift
+++ CarRecognition/Source Files/Common/Models/RecognizedCar.swift
@@ -4,7 +4,16 @@
 //
 
 
-internal struct RecognizedCar {
+internal struct RecognizedCar: Equatable {
     let car: String
     let confidence: Float
+    
+    /// Description with confidence formatted in percents
+    var descriptionWithConfidence: String {
+        return "\(car)\n(\(CRNumberFormatter.percentageFormatted(confidence)))"
+    }
+    
+    public static func == (lhs: RecognizedCar, rhs: RecognizedCar) -> Bool {
+        return lhs.car == rhs.car
+    }
 }
--- CarRecognition/Source Files/Modules/Live video/LiveVideoViewController.swift
+++ CarRecognition/Source Files/Modules/Live video/LiveVideoViewController.swift
@@ -8,9 +8,11 @@ import UIKit
 import SpriteKit
 import ARKit
 
-internal final class LiveVideoViewController: TypedViewController<LiveVideoView>, ARSessionDelegate {
+internal final class LiveVideoViewController: TypedViewController<LiveVideoView>, ARSessionDelegate, ARSKViewDelegate {
     
     private let classificationService = CarClassificationService()
+    
+    private var addedAnchors: [ARAnchor: RecognizedCar] = [:]
     
     /// SeeAlso: UIViewController
     override func viewDidLoad() {
@@ -18,6 +20,7 @@ internal final class LiveVideoViewController: TypedViewController<LiveVideoView>
         customView.checkDetailsButton.addTarget(self, action: #selector(didTapCheckDetailsButton), for: .touchUpInside)
         classificationService.completionHandler = { [weak self] result in
             self?.handleRecognition(result: result)
+            self?.handleRecognitionInAR(result: result)
         }
     }
     
@@ -40,10 +43,12 @@ internal final class LiveVideoViewController: TypedViewController<LiveVideoView>
     }
     
     private func setupSession() {
+        customView.previewView.delegate = self
         customView.previewView.session.delegate = self
         
         let configuration = ARWorldTrackingConfiguration()
         if #available(iOS 11.3, *) {
+            configuration.planeDetection = [.vertical]
             configuration.isAutoFocusEnabled = true
         }
         customView.previewView.session.run(configuration)
@@ -54,8 +59,34 @@ internal final class LiveVideoViewController: TypedViewController<LiveVideoView>
         
         let labels = [customView.modelFirstLabel, customView.modelSecondLabel, customView.modelThirdLabel]
         for (index, element) in result.cars.prefix(3).enumerated() {
-            labels[index].text = "\(element.car)\n(\(CRNumberFormatter.percentageFormatted(element.confidence)))"
+            labels[index].text = element.descriptionWithConfidence
         }
+    }
+    
+    private func handleRecognitionInAR(result: CarRecognizeResponse) {
+        guard
+            let mostConfidentRecognition = result.cars.first,
+            mostConfidentRecognition.car != "not car",
+            mostConfidentRecognition.confidence >= 0.99,
+            !addedAnchors.contains(where: { $0.value == mostConfidentRecognition})
+        else {
+            return
+        }
+        let hitTests = customView.previewView.hitTest(CGPoint(x: 0.5, y: 0.5), types: [.featurePoint])
+        guard let possibleCarHit = hitTests.first(where: { $0.distance > 0.4 }) else {
+            print("Hit test failed or detected object is to close to the phone")
+            return
+        }
+        guard let lastCapturedFrame = customView.previewView.session.currentFrame else {
+            print("No recent AR frame found")
+            return
+        }
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = Float(-possibleCarHit.distance)
+        let transform = simd_mul(lastCapturedFrame.camera.transform, translation)
+        let anchor = ARAnchor(transform: transform)
+        addedAnchors[anchor] = mostConfidentRecognition
+        customView.previewView.session.add(anchor: anchor)
     }
     
     @objc private func didTapCheckDetailsButton() {
@@ -67,5 +98,17 @@ internal final class LiveVideoViewController: TypedViewController<LiveVideoView>
     /// SeeAlso: ARSessionDelegate
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         classificationService.perform(on: frame.capturedImage)
+    }
+    
+    /// SeeAlso: ARSKViewDelegate
+    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+        guard let model = addedAnchors[anchor] else { return nil }
+        let labelNode = SKLabelNode(text: model.car)
+        labelNode.horizontalAlignmentMode = .center
+        labelNode.verticalAlignmentMode = .center
+        labelNode.fontName = UIFont.boldSystemFont(ofSize: 70).fontName
+        labelNode.fontSize = 70
+        labelNode.fontColor = .red
+        return labelNode
     }
 }
