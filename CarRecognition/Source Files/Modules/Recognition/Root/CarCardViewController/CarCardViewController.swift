1ae95a66e9651bb46bb2d9b7cd727ba3602427a6
Michał Warchał <michal.warchal@netguru.co>
2018-07-05 10:26:10 +0200

    Improve exit animation

--- CarRecognition/Source Files/Modules/Recognition/Root/CarCardViewController/CarCardViewController.swift
+++ CarRecognition/Source Files/Modules/Recognition/Root/CarCardViewController/CarCardViewController.swift
@@ -8,12 +8,17 @@ import UIKit
 
 internal final class CarCardViewController: TypedViewController<CarCardView> {
 
+    struct Constants {
+        static let entryPosition = UIScreen.main.bounds.maxY / 2
+        static let exitPosition = UIScreen.main.bounds.maxY
+    }
+
     /// Animator for entry animations
-    private let exitAnimator = UIViewPropertyAnimator(duration: 1.0, curve: .linear)
-    
-    /// Animator for exit animations
     private let entryAnimator = UIViewPropertyAnimator(duration: 1.0, curve: .linear)
-    
+
+    /// Animator for exit animations
+    private let exitAnimator = UIViewPropertyAnimator(duration: 0.7, curve: .linear)
+
     /// Car instance
     private let car: Car
 
@@ -40,13 +45,18 @@ internal final class CarCardViewController: TypedViewController<CarCardView> {
         case .began:
             exitAnimator.addAnimations {
                 let frame = self.view.frame
-                self.view.frame = CGRect(x: 0, y: UIScreen.main.bounds.maxY + frame.height, width: frame.width, height: frame.height)
+                self.view.frame = CGRect(x: 0, y: Constants.exitPosition, width: frame.width, height: frame.height)
             }
             exitAnimator.pausesOnCompletion = true
             exitAnimator.pauseAnimation()
         case .changed:
             let translation = recognizer.translation(in: view)
-            exitAnimator.fractionComplete = translation.y / view.frame.height
+            exitAnimator.fractionComplete = translation.y / Constants.entryPosition
+            
+            if recognizer.direction == .bottomToTop, exitAnimator.fractionComplete == 0 {
+                exitAnimator.stopAnimation(true)
+                view.frame = CGRect(x: 0, y: Constants.entryPosition, width: view.frame.width, height: view.frame.height)
+            }
         case .ended:
             exitAnimator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
         default: break
@@ -57,8 +67,7 @@ internal final class CarCardViewController: TypedViewController<CarCardView> {
     func animateIn() {
         entryAnimator.addAnimations {
             let frame = self.view.frame
-            let yComponent = UIScreen.main.bounds.height / 2
-            self.view.frame = CGRect(x: 0, y: yComponent, width: frame.width, height: frame.height)
+            self.view.frame = CGRect(x: 0, y: Constants.entryPosition, width: frame.width, height: frame.height)
         }
         entryAnimator.startAnimation()
     }
