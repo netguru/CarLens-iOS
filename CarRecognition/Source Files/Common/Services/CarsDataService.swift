5e037ef0def21c12628c047f12f7c5af37ef2d90
Michał Kwiecień <michal.kwiecien@netguru.pl>
2018-07-10 14:27:06 +0200

    Added temporary mapper

--- CarRecognition/Source Files/Common/Services/CarsDataService.swift
+++ CarRecognition/Source Files/Common/Services/CarsDataService.swift
@@ -11,6 +11,7 @@ internal final class CarsDataService {
     private let databaseService = CarsDatabaseService()
     
     func map(classifierLabel: String) -> Car? {
+        // TODO: Temporary, to be removed after model change
         let mappedClassifierLabel = mapToCorrectClassifierLabel(classifierLabel: classifierLabel)
         
         guard var car = localDataService.cars.first(where: { $0.id == mappedClassifierLabel }) else { return nil }
@@ -18,10 +19,25 @@ internal final class CarsDataService {
         return car
     }
     
+    // TODO: Temporary function, to ve removed after model change
     private func mapToCorrectClassifierLabel(classifierLabel: String) -> String {
         switch classifierLabel {
         case "toyota camry":
             return "ToyotaCamry"
+        case "honda civic":
+            return "HondaCivic"
+        case "toyota corolla":
+            return "ToyotaCorolla"
+        case "ford fiesta":
+            return "FordFiesta"
+        case "volkswagen golf":
+            return "VolkswagenGolf"
+        case "nissan qashqai":
+            return "NissanQashqai"
+        case "volkswagen passat":
+            return "VolkswagenPassat"
+        case "volkswagen tiguan":
+            return "VolkswagenTiguan"
         default:
             return classifierLabel
         }
