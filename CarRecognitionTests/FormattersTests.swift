c240765aba2652e2b6972d81f7260b0b0b12e463
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2018-07-23 13:42:24 +0200

    Add time interval test

---
+++ CarRecognitionTests/FormattersTests.swift
@@ -0,0 +1,23 @@
+//
+//  FormattersTests.swift
+//  CarRecognitionTests
+//
+
+
+@testable import CarRecognition
+import XCTest
+
+class FormattersTests: XCTestCase {
+    
+    func testNumberFormatterFor10Percent() {
+        let givenValue = Float(0.1)
+        let percentageFormattedString = CRNumberFormatter.percentageFormatted(givenValue)
+        XCTAssertEqual(percentageFormattedString, "\(Int(givenValue * 100))%", "Should return percentage formatted string from float.")
+    }
+    
+    func testTimeFormatterFor50Miliseconds() {
+        let timeInterval: TimeInterval = 0.05
+        let formattedTime = CRTimeFormatter.intervalMilisecondsFormatted(timeInterval)
+        XCTAssertEqual(formattedTime, "50 ms", "Should return miliseconds formatted string from time interval.")
+    }
+}
