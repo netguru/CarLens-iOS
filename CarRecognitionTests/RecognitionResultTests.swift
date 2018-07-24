d5fc8678b29c4f76b773f3bc2159bbf02f5cbb43
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2018-07-24 11:29:10 +0200

    Improve tests for Recognition Result

--- CarRecognitionTests/RecognitionResultTests.swift
+++ CarRecognitionTests/RecognitionResultTests.swift
@@ -15,24 +15,10 @@ final class RecognitionResultTests: XCTestCase {
         static let unknown = "unknown label"
     }
     
-    var sut: RecognitionResult?
-    var localCarDataService: LocalCarsDataService!
-    var carsDataService: CarsDataService!
-    
-    override func setUp() {
-        super.setUp()
-        localCarDataService = LocalCarsDataService()
-        carsDataService = CarsDataService()
-    }
-
-    override func tearDown() {
-        localCarDataService = nil
-        carsDataService = nil
-        super.tearDown()
-    }
+    var sut: RecognitionResult!
     
     func testInitizationForCar() {
-        let car = localCarDataService.cars.first
+        let car = LocalCarsDataService().cars.first
         XCTAssertNotNil(car, "Local car data base should not be empty.")
         guard let firstCar = car else { return }
         testResult(for: firstCar.id)
@@ -40,25 +26,25 @@ final class RecognitionResultTests: XCTestCase {
         XCTAssertEqual(result.recognition, RecognitionResult.Recognition.car(firstCar), "Recognition Result should return car for its label.")
     }
     
-    func testInitizationForOtherCar() {
+    func testInitializationForOtherCar() {
         testResult(for: Labels.otherCar)
         guard let result = sut else { return }
         XCTAssertEqual(result.recognition, RecognitionResult.Recognition.otherCar, "Recognition Result should return otherCar enum type for label that's not in local data base.")
     }
     
-    func testInitizationForNotCar() {
+    func testInitializationForNotCar() {
         testResult(for: Labels.notCar)
         guard let result = sut else { return }
         XCTAssertEqual(result.recognition, RecognitionResult.Recognition.notCar, "Recognition Result should return notCar enum type for label 'not car'.")
     }
    
-    func testInitizationForUnknownLabel() {
-        sut = RecognitionResult(label: Labels.unknown, confidence: 0.9, carsDataService: carsDataService)
+    func testInitializationForUnknownLabel() {
+        sut = RecognitionResult(label: Labels.unknown, confidence: 0.9, carsDataService: CarsDataService())
         XCTAssertNil(sut, "Recognition Result should fail to initialize for an unknown label.")
     }
     
     private func testResult(for label: String) {
-        sut = RecognitionResult(label: label, confidence: 0.9, carsDataService: carsDataService)
+        sut = RecognitionResult(label: label, confidence: 0.9, carsDataService: CarsDataService())
         XCTAssertNotNil(sut, "Recognition Result should not be empty.")
     }
 }
