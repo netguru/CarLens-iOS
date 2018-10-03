f26dfd60a95f4fc7601f5c63401a9884b93a79ba
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2018-10-03 14:12:36 +0200

    Add constants

--- CarRecognition/Source Files/Modules/Onboarding/Root/Indicator Animation/OnboardingIndicatorAnimationView.swift
+++ CarRecognition/Source Files/Modules/Onboarding/Root/Indicator Animation/OnboardingIndicatorAnimationView.swift
@@ -9,10 +9,31 @@ import UIKit
 
 internal class OnboardingIndicatorAnimationView: View, ViewSetupable {
     
+    struct Constants {
+        static let dotWidth: CGFloat = 5
+        static let dotExtendedWidth = dotWidth * 4
+        static let spacing: CGFloat = 5
+    }
+    
+    struct Frames {
+        struct Extended {
+            static let first = CGRect(x: 0, y: 0, width: Constants.dotExtendedWidth, height: Constants.dotWidth)
+            static let second = CGRect(x: Constants.dotWidth + Constants.spacing, y: 0, width: Constants.dotExtendedWidth, height: Constants.dotWidth)
+            static let third = CGRect(x: Constants.dotWidth * 2 + Constants.spacing * 2, y: 0, width: Constants.dotExtendedWidth, height: Constants.dotWidth)
+        }
+        
+        struct Initial {
+            static let first = CGRect(x: 0, y: 0, width: Constants.dotWidth, height: Constants.dotWidth)
+            static let secondFromFirst = CGRect(x: Constants.dotExtendedWidth + Constants.spacing, y: 0, width: Constants.dotWidth, height: Constants.dotWidth)
+            static let secondFromThird = CGRect(x: Constants.dotWidth + Constants.spacing, y: 0, width: Constants.dotWidth, height: Constants.dotWidth)
+            static let third = CGRect(x: Constants.dotExtendedWidth + Constants.dotWidth + Constants.spacing * 2, y: 0, width: Constants.dotWidth, height: Constants.dotWidth)
+        }
+    }
+    
     let firstDotView = UIView().layoutable()
     let secondDotView = UIView().layoutable()
     let thirdDotView = UIView().layoutable()
-//    let dotsViews: [UIView] = Array.init(repeating: UIView().layoutable(), count: 3)
+    let dotViews: [UIView] = Array.init(repeating: UIView().layoutable(), count: 3)
     let dotWidth: CGFloat = 5
     let dotExtendedWidth: CGFloat = 20
     let spacing: CGFloat = 5
@@ -30,30 +51,30 @@ internal class OnboardingIndicatorAnimationView: View, ViewSetupable {
         case .fromFirstToSecond:
             animations = {
                 self.firstDotView.backgroundColor = UIColor.crOnboardingLightOrange
-                self.firstDotView.frame = CGRect(x: 0, y: 0, width: self.dotWidth, height: self.dotWidth)
-                self.secondDotView.frame = CGRect(x: self.dotWidth + self.spacing, y: 0, width: self.dotExtendedWidth, height: self.dotWidth)
+                self.firstDotView.frame = Frames.Initial.first
+                self.secondDotView.frame = Frames.Extended.second
                 self.secondDotView.backgroundColor = UIColor.crOnboardingDeepOrange
-            }
-        case .fromSecondToFirst:
-            animations = {
-                self.secondDotView.backgroundColor = UIColor.crOnboardingLightOrange
-                self.secondDotView.frame = CGRect(x: self.dotExtendedWidth + self.spacing, y: 0, width: self.dotWidth, height: self.dotWidth)
-                self.firstDotView.frame = CGRect(x: 0, y: 0, width: self.dotExtendedWidth, height: self.dotWidth)
-                self.firstDotView.backgroundColor = UIColor.crOnboardingDeepOrange
             }
         case .fromSecondToThird:
             animations = {
                 self.secondDotView.backgroundColor = UIColor.crOnboardingLightOrange
-                self.secondDotView.frame = CGRect(x: self.dotWidth + self.spacing, y: 0, width: self.dotWidth, height: self.dotWidth)
-                self.thirdDotView.frame = CGRect(x: self.dotWidth * 2 + self.spacing * 2, y: 0, width: self.dotExtendedWidth, height: self.dotWidth)
+                self.secondDotView.frame = Frames.Initial.secondFromThird
+                self.thirdDotView.frame = Frames.Extended.third
                 self.thirdDotView.backgroundColor = UIColor.crOnboardingDeepOrange
             }
         case .fromThirdToSecond:
             animations = {
                 self.thirdDotView.backgroundColor = UIColor.crOnboardingLightOrange
-                self.thirdDotView.frame = CGRect(x: self.dotExtendedWidth + self.dotWidth + self.spacing * 2, y: 0, width: self.dotWidth, height: self.dotWidth)
-                self.secondDotView.frame = CGRect(x: self.dotWidth + self.spacing, y: 0, width: self.dotExtendedWidth, height: self.dotWidth)
+                self.thirdDotView.frame = Frames.Initial.third
+                self.secondDotView.frame = Frames.Extended.second
                 self.secondDotView.backgroundColor = UIColor.crOnboardingDeepOrange
+            }
+        case .fromSecondToFirst:
+            animations = {
+                self.secondDotView.backgroundColor = UIColor.crOnboardingLightOrange
+                self.secondDotView.frame = Frames.Initial.secondFromFirst
+                self.firstDotView.frame = Frames.Extended.first
+                self.firstDotView.backgroundColor = UIColor.crOnboardingDeepOrange
             }
         }
         guard let animationsArray = animations else { return }
@@ -65,9 +86,9 @@ internal class OnboardingIndicatorAnimationView: View, ViewSetupable {
     }
     
     func setupConstraints() {
-        firstDotView.frame = CGRect(x: 0, y: 0, width: dotExtendedWidth, height: dotWidth)
-        secondDotView.frame = CGRect(x: dotExtendedWidth + spacing, y: 0, width: dotWidth, height: dotWidth)
-        thirdDotView.frame = CGRect(x: dotExtendedWidth + spacing * 2 + dotWidth, y: 0, width: dotWidth, height: dotWidth)
+        firstDotView.frame = Frames.Initial.first
+        secondDotView.frame = Frames.Initial.secondFromFirst
+        thirdDotView.frame = Frames.Initial.third
     }
     
     func setupProperties() {
