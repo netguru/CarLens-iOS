68ce966ac1095decc03dea18a2282ec8e3f24d75
Anna-Mariia Shkarlinska <anna-mariia.shkarlinska@netguru.co>
2018-10-09 15:30:18 +0200

    Update tests

--- CarRecognitionTests/Tests/Services/InputNormalizationServiceTests.swift
+++ CarRecognitionTests/Tests/Services/InputNormalizationServiceTests.swift
@@ -11,51 +11,45 @@ final class InputNormalizationServiceTests: XCTestCase {
     
     var sut: InputNormalizationService!
     
-    func testInitialValueWith30LastValues() {
-        sut = InputNormalizationService(numberOfValues: 30)
-        let result = sut.normalize(value: 0.3)
-        XCTAssertEqual(result, 0.01, accuracy: 0.01)
+    var carsDataService: CarsDataService!
+    
+    override func setUp() {
+        super.setUp()
+        carsDataService = CarsDataService()
+        sut = InputNormalizationService(numberOfValues: 3, carsDataService: carsDataService)
     }
     
-    func testInitialValueWith10LastValues() {
-        sut = InputNormalizationService(numberOfValues: 10)
-        let result = sut.normalize(value: 0.3)
-        XCTAssertEqual(result, 0.03, accuracy: 0.01)
-    }
-    
-    func test15thWith30LastValues() {
-        sut = InputNormalizationService(numberOfValues: 30)
-        var result = 0.0
-        for _ in 0..<15 {
-            result = sut.normalize(value: 0.5)
+    func testWithOneValue() {
+        guard let recognitionResult = RecognitionResult(label: RecognitionResultTests.Labels.notCar, confidence: 0.75, carsDataService: carsDataService) else {
+            XCTFail("Recognition result can't be nil")
+            return
         }
-        XCTAssertEqual(result, 0.25, accuracy: 0.01)
+        let result = sut.normalizeConfidence(from: [recognitionResult])
+        XCTAssertNil(result, "Result should be nil if the recognition results count is less then number of values for a normalization")
     }
     
-    func test30thWith30LastValues() {
-        sut = InputNormalizationService(numberOfValues: 30)
-        var result = 0.0
-        for _ in 0..<30 {
-            result = sut.normalize(value: 0.5)
+    func testAfterAddingValues() {
+        testWithOneValue()
+        guard let firstResult = RecognitionResult(label: RecognitionResultTests.Labels.otherCar, confidence: 0.2, carsDataService: carsDataService),
+            let secondResult = RecognitionResult(label: RecognitionResultTests.Labels.otherCar, confidence: 0.6, carsDataService: carsDataService) else {
+            XCTFail("Recognition results can't be nil")
+            return
         }
-        XCTAssertEqual(result, 0.5, accuracy: 0.01)
+        let result = sut.normalizeConfidence(from: [firstResult, secondResult])
+        XCTAssertEqual(result?.confidence, 0.75, "Result confidence should be the highest average from all of the previous results")
+        XCTAssertEqual(result?.label, RecognitionResultTests.Labels.notCar, "Result confidence should be the highest average from all of the previous results")
     }
     
-    func test5thWith10LastValues() {
-        sut = InputNormalizationService(numberOfValues: 10)
-        var result = 0.0
-        for _ in 0..<5 {
-            result = sut.normalize(value: 0.5)
+    func testWithThreeValuesOutOfThree() {
+        guard let firstResult = RecognitionResult(label: RecognitionResultTests.Labels.notCar, confidence: 0.7, carsDataService: carsDataService),
+            let secondResult = RecognitionResult(label: RecognitionResultTests.Labels.notCar, confidence: 0.5, carsDataService: carsDataService),
+            let thirdResult = RecognitionResult(label: RecognitionResultTests.Labels.otherCar, confidence: 0.2, carsDataService: carsDataService) else {
+            XCTFail("Recognition results can't be nil")
+            return
         }
-        XCTAssertEqual(result, 0.25, accuracy: 0.01)
+        let result = sut.normalizeConfidence(from: [firstResult, secondResult, thirdResult])
+        XCTAssertEqual(result?.confidence, 0.6, "Result confidence should be the highest average")
+        XCTAssertEqual(result?.label, RecognitionResultTests.Labels.notCar, "Result confidence should be the highest average")
     }
     
-    func test10thWith10LastValues() {
-        sut = InputNormalizationService(numberOfValues: 10)
-        var result = 0.0
-        for _ in 0..<10 {
-            result = sut.normalize(value: 0.5)
-        }
-        XCTAssertEqual(result, 0.5, accuracy: 0.01)
-    }
 }
